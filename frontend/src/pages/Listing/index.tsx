6069ea693b3fe0714f6d95359e54cfd55f79f745
andrephellipe <andre.eter.10@gmail.com>
2022-05-13 19:48:14 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,8 +2,16 @@ import './styles.css';
 
 import Pagination from "components/Pagination";
 import MovieCard from 'components/Moviecard';
+import axios from 'axios';
+import { BASE_URL } from 'utils/requests';
 
 function Listing(){
+
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        });
 
     return(
         <>
